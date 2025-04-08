c086ebf6c953d8a36c3c03b04a41ed08fda784d8
Sergiu <sergiu.neag@nia-medtech.com>
2025-04-08 18:37:21 +0300

    Add login

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from user.views import RegularUserList, UserRegisterView
+from user.views import AuthentificationView, RegularUserList, UserRegisterView
 
 router = DefaultRouter()
-router.register('', RegularUserList, basename='user')
+router.register("", RegularUserList, basename="user")
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('dj-rest-auth/registration/', UserRegisterView.as_view(), name='register'),
-
+    path("", include(router.urls)),
+    path("dj-rest-auth/registration/",
+         UserRegisterView.as_view(),
+         name="register"),
+    path("dj-rest-auth/login/",
+         AuthentificationView.as_view(),
+         name="rest_login"),
 ]
